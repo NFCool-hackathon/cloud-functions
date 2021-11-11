87f891b3042272dbcd0cb34910f1dbd0aab77ece
Samuel VIDEAU <samuelvideau@yahoo.fr>
2021-11-11 12:07:10 +0100

    feat: response body

--- functions/src/2FA.ts
+++ functions/src/2FA.ts
@@ -29,6 +29,7 @@ export const checkPhoneVerification = functions.region("europe-west1").https.onR
   const pin = req.query.pin as string;
 
   if (!pin || !tokenId || !unitId) {
+    console.error("You need to send all parameters");
     res.status(400).send({message: "You need to send all parameters"});
   }
 
@@ -37,17 +38,32 @@ export const checkPhoneVerification = functions.region("europe-west1").https.onR
   if (sale.data()?.phoneNumber) {
     phoneNumber = sale.data()?.phoneNumber;
   } else {
+    console.error("No phone number is associated with this token");
     res.status(404).send({message: "No phone number is associated with this token"});
   }
 
-  const twilioRes = await client.verify.services("VAae61bb31ff07b220a4b56ad7585e543b").verificationChecks.create({to: phoneNumber, code: pin});
+  let twilioRes;
+  try {
+    twilioRes = await client.verify.services("VAae61bb31ff07b220a4b56ad7585e543b").verificationChecks.create({to: phoneNumber, code: pin});
+    console.log("DEBUG TWILIO RES");
+    console.log(JSON.stringify(twilioRes));
+  } catch (e) {
+    console.log("DEBUG TWILIO ERROR");
+    console.error(JSON.stringify(twilioRes));
+    res.status(500).send(e);
+  }
 
-  console.log("DEBUG TWILIO RES");
-  console.log(JSON.stringify(twilioRes));
-
-  if (twilioRes.valid) {
+  if (twilioRes && twilioRes.valid) {
     res.status(200).send({valid: true});
   } else {
     res.status(200).send({valid: false});
   }
 });
+
+// const awaitFn = async (milli: number) => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve();
+//     }, milli);
+//   });
+// };
