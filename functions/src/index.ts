a7dcdfbe3a4e189df2df93c6d3ab43bbbff20767
Samuel VIDEAU <samuel.videau@efrei.net>
2021-11-24 14:21:00 +0100

    delete: uploadImageOnIPFS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,8 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import {checkPhoneVerification, sendPhoneVerification} from "./2FA";
-import {uploadImageOnIPFS} from "./ipfs-storage";
 
 admin.initializeApp(functions.config().firebase);
 
 exports.sendPhoneVerification = sendPhoneVerification;
 exports.checkPhoneVerification = checkPhoneVerification;
-exports.uploadImageOnIPFS = uploadImageOnIPFS;
